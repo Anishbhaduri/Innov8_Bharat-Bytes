4ce61e06b2d1c742618956e3e17cd3921d126070
anishbhaduri <anishbhaduri100@gmail.com>
2024-09-15 08:38:52 +0000

    Removed API key from public repo

--- firebase.js
+++ firebase.js
@@ -6,16 +6,5 @@
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBpuHIpAypAqby5d0QEfjCeNCn-JwhlpSY",
-    authDomain: "bharatbytesdev.firebaseapp.com",
-    projectId: "bharatbytesdev",
-    storageBucket: "bharatbytesdev.appspot.com",
-    messagingSenderId: "791544305579",
-    appId: "1:791544305579:web:960860009b576ff1e4e4ee",
-    measurementId: "G-TKS2R26Z8S"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  
+  // ENTER YOUR FIREBASE CREDENTIALS HERE. IT IS NOT INCLUDED IN THIS REPO //
